2cc0ede0fdaa56fc88a9f5adf6e66173d05504b1
surexspd <suryaprakashdash76@gmail.com>
2025-10-18 00:48:23 +0530

    Add Initial Files

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("login").addEventListener("click",()=>{
+    window.location.href="../loginpage.html"
+})
+document.getElementById("Cheak").addEventListener("click",()=>{
+    window.location.href="../loginpage.html"
+})
+
